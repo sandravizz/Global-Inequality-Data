1770dc3306a25c5d771733e47b723e283c618165
Sandra Becker <sandra.becker1982@gmail.com>
2024-03-22 17:07:40 +0000

    update

--- js/script2.js
+++ js/script2.js
@@ -1,27 +1,29 @@
 
 async function fetchAndTransform(name) {
+  
   const data = await aq.load(`../data/countries/WID_data_${name}.csv`, { using: aq.fromCSV });
+
+  console.log("fetched", name);
 
   data.derive({
     unit: (d) => op.slice(d.variable, 0, 1)
   })
-    .filter((d) => d.unit === "g" && d.year > "1994" && d.variable === "ghwealj992")
-  // .print(5);
+    .filter((d) => d.unit === "g" && d.year > "1994" && d.variable ===  "ghwealj992");
+    // .print(5);
 
   let data_g = data.derive({
     unit: (d) => op.slice(d.variable, 0, 1)
   })
-    .filter((d) => d.unit === "g" && d.year > "1994" && d.variable === "ghwealj992")
+    .filter((d) => d.unit === "g" && d.year > "1994" && d.variable ===  "ghwealj992")
     .objects();
 
   return data_g;
 }
 
-
 async function main() {
 
   // list of countries to fetch data from
-  const countryNames = ["PT", "DE", "US", "GB", "SE", "AE"]
+  const countryNames = ["PT", "SE", "AE", "US", "GB", "DE", "AT"]
 
   // fetch and transform data -> return promise
   const promises = countryNames.map(d => fetchAndTransform(d))
@@ -30,11 +32,17 @@ async function main() {
   const result = await Promise.all(promises)
 
   // Flatten the array of arrays
-  console.log(result.flatMap(d => d))
+  let data_total = result.flatMap(d => d);
+  console.log(data_total);
+
+const string = d3.csvFormat(data_total, ["unit", "year", "country", "variable", "value"]);
+  console.log(string);
 
 }
 
 main();
+
+
 
 
 
