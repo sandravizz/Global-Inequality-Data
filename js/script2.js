abeb5977e3951e961fbe9e3a1f65fb7064d3accc
Sandra Becker <sandra.becker1982@gmail.com>
2024-03-25 18:33:02 +0000

    update all countires

--- js/script2.js
+++ js/script2.js
@@ -1,8 +1,7 @@
 
 async function fetchAndTransform(name) {
   
-  const data = await aq.load(`../data/countries/WID_data_${name}.csv`, { using: aq.fromCSV });
-
+  const data = await aq.load(`../data/countries/WID_data_${name}.csv`, { delimiter: ';' }, { using: aq.fromCSV });
   console.log("fetched", name);
 
   data
@@ -22,7 +21,17 @@ async function fetchAndTransform(name) {
 async function main() {
 
   // list of countries to fetch data from
-  const countryNames = ["AD", "AE", "AF", "AG", "AI", "AT", "PT", "SE", "US", "GB", "DE", "BE", "ZW", "CH", "CG"]
+  const countryNames = ["AD", "AE", "AF", "AG", "AI", "AL", "AM", "AO", "AR", "AT", "AU", "AZ", 
+  "BA", "BD", "BE", "BF", "BG", "BH", "BI", "BJ", "BN", "BO", "BQ", "BR", "BS", "BT", "BW", "BZ", 
+  "CA", "CF", "CG", "CH", "CI", "CK", "CL", "CM", "CN", "CO", "CR", "CU", "CV", "CY", "CZ",
+  "DE", "DJ", "DK", "DO", "DZ", "EC", "EE", "EG", "ER", "ES", "ET", "FI", "FR", 
+  "GA", "GB", "GD", "GE", "GG", "GH", "GM", "GN", "GQ", "GR", "GT", "GW", "GY", 
+  "HK", "HN", "HR", "HT", "HU", "ID", "IE", "IL", "IN", "IQ", "IR", "IS", "IT", "JM", "JO", "JP", 
+  "KE", "KG", "KH", "KM", "KP", "KR", "KS", "KW", "KY", "KZ",
+  "LA", "LB", "LC", "LI", "LK", "LR", "LS", "LT", "LU", "LV", "LY",
+  "MA", "MD", "ME", "MG", "MK", "ML", "MM", "MN", "MO", "MR", "MT",
+  "MU", "MV", "MW", "MX", "MY", "MZ", "NA", "NE", "NG", "NI", "NL",
+  "NO", "NP", "NZ", "OM", "PA", "PE", "PG", "PH", "PK", "PL", "PR"];
 
   // fetch and transform data -> return promise
   const promises = countryNames.map(d => fetchAndTransform(d))
@@ -34,9 +43,9 @@ async function main() {
   let data_total = result.flatMap(d => d);
   console.log(data_total);
 
-  // Save in the csv format
-  // const string = d3.csvFormat(data_total, ["unit", "year", "country", "variable", "value"]);
-  // console.log(string);
+  // // Save in the csv format
+  const string = d3.csvFormat(data_total, ["unit", "year", "country", "variable", "value"]);
+  console.log(string);
 
 // --------------------------------------
 //  Margin and canvas
@@ -75,11 +84,11 @@ let x = d3.scaleLinear()
     .domain([0, d3.max(data_total, d => d.value)])
     .range([0, innerwidth]);
 
-console.log(data_total.map(d => d.country));
+// console.log(data_total.map(d => d.country));
 
-let c = d3.scaleOrdinal()
-    .domain(["AD", "AE", "AF", "AG", "AI", "AT","PT", "SE", "US", "GB", "DE", "BE", "ZW", "CH", "CG"])
-    .range(["white", "white", "white", "white", "white", "white", "white", "white", "red", "white", "white"]);
+// let c = d3.scaleOrdinal()
+//     .domain(["AD", "AE", "AF", "AG", "AI", "AT","PT", "SE", "US", "GB", "DE", "BE", "ZW", "CH", "CG"])
+//     .range(["white", "white", "white", "white", "white", "white", "white", "white", "red", "white", "white"]);
 
 // let r2 = d3.scaleSqrt()
 //     .domain(d3.extent(data_total, d => d.value))
@@ -114,9 +123,9 @@ innerChart
     .attr("class", "circle") 
     .attr("cx", (d) => x(d.value))
     .attr("cy", (d) => y(d.year))
-    .attr("r", 6)
+    .attr("r", 5)
     .attr("opacity", 0.5)  
-    .attr("fill", (d) => c(d.country));
+    .attr("fill", "white");
 
 }
 
