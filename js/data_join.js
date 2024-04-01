cc5011482a43b7b1fcb391a0678a634022103bc6
Patrick Wojda <pewe@PPP.lan>
2024-04-01 19:39:50 +0200

    add missing years to GINI data

--- js/data_join.js
+++ js/data_join.js
@@ -1,44 +1,44 @@
-
-// async function loading() {
-  
-//   const info_countries = await aq.load(`../data/WID_countries.csv`, { delimiter: ',' }, { using: aq.fromCSV });
-//   console.log("fetched", info_countries);
-
-//   const gini_countries = await aq.load(`../data/gini_1995-2022_all.csv`, { delimiter: ',' }, { using: aq.fromCSV });
-//   console.log("fetched", gini_countries);
-
-//   gini_countries.join(info_countries, ["country", "alpha2"]);
-
-//   let gini_all = gini_countries.join(info_countries, ["country", "alpha2"])
-//   .objects();
-//   console.log(gini_all);
-
-//   const string2 = d3.csvFormat(gini_all, ["unit", "year", "country", "variable", "value", "shortname", "titlename", "region", "region2"]);
-//   console.log(string2);
-
-// };
-
-// loading();
-
-
 async function loading2() {
-  
+  console.log('---> start processing')
+
+  // Load years and Gini data from CSV files
   const years = await aq.load(`../data/years.csv`, { using: aq.fromCSV });
-  console.log("fetched", years);
-
   const gini_all = await aq.load(`../data/gini_all.csv`, { delimiter: ',' }, { using: aq.fromCSV });
-  console.log("fetched", gini_all);
 
-  let test = years.join_full(gini_all, ["year", "year"])
-  .objects();
-  console.log(test);
+  // Extract an array of years from the loaded data
+  const yearsArray = years.objects().map(d => d.year);
 
-  const string2 = d3.csvFormat(gini_all, ["unit", "year", "country", "variable", "value", "shortname", "titlename", "region", "region2"]);
-  console.log(string2);
+  // Construct an object country as key and data in a Set 
+  const countriesObject = gini_all.objects().reduce((acc, d) => {
+    // If the country already exists in the object, add the new data to its set
+    // Otherwise, create a new set with the current data
+    acc[d.country] = acc[d.country] ? acc[d.country].add(d) : new Set([d]);
+    return acc;
+  }, {});
 
+  // Iterate over each country's data to find and add missing years
+  Object.values(countriesObject).forEach(countryData => {
+    const countryYears = Array.from(countryData).map(d => d.year);
+    const missingYears = yearsArray.filter(y => !countryYears.includes(y));
+
+    // For each missing year, create a new data entry with null value and add it to the country's set
+    missingYears.forEach(year => {
+      const missingYearData = { year: year, value: null }; // Assuming the structure of missing data
+      countryData.add(missingYearData);
+    });
+  });
+
+
+  // Log the final countriesObject with all data, including added missing years
+  console.log(Object.values(countriesObject).flatMap(d => Array.from(d)))
+
+  console.log('<--- finsished processing')
 };
 
-loading2(); 
+loading2();
+
+
+
 
 
 
