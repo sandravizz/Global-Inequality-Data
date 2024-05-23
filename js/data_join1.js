f2aa24c670457f5083c53755c612df7b4183bb21
Sandra Becker <sandra.becker1982@gmail.com>
2024-05-23 13:17:42 +0100

    update loading

--- js/data_join1.js
+++ js/data_join1.js
@@ -2,20 +2,20 @@ async function loading1() {
 
     console.log('---> start processing')
 
-    const gini_income = await aq.load(`../data/output_files/income/1.csv`, { delimiter: ',' }, { using: aq.fromCSV });
-    console.log(gini_income.objects());
+    const population = await aq.load(`../data/output_files/population/1.csv`, { delimiter: ',' }, { using: aq.fromCSV });
+    console.log(population.objects());
 
     const WID_countries = await aq.load(`../data/WID_countries.csv`, { delimiter: ',' }, { using: aq.fromCSV });
     console.log(WID_countries.objects());
 
-    let gini_income_all = gini_income
+    let population_all = population
     .join(WID_countries, ['country', 'alpha2'])
     .objects(); 
 
-    console.log(gini_income_all); 
+    console.log(population_all); 
 
-    let string2 = d3.csvFormat(gini_income_all, ["year", "country", "variable", "value", "region", "region2", "shortname", "titlename"]);
-    console.log(string2);
+    let string3 = d3.csvFormat(population_all, ["year", "country", "variable", "value", "region", "region2", "shortname", "titlename"]);
+    console.log(string3);
 
     console.log('<--- finsished processing')
 };
--- js/load_generalised_all_data.js
+++ js/load_generalised_all_data.js
@@ -1,54 +1,61 @@
-async function fetchAndTransform(name, variableToGet, yearFrom) {
+async function fetchAndTransform(name, variableToGetArray, yearFrom) {
 
     const data = await aq.load(`../data/countries/WID_data_${name}.csv`, { delimiter: ';' }, { using: aq.fromCSV });
-    console.log("fetched", name);
 
-    const filteredTable = data.filter(aq.escape(d => d.variable === variableToGet));
-
-    if (filteredTable.objects().length > 0) {
-        let data_g = data
-            .filter(aq.escape((d) => d.year >= yearFrom && d.variable === variableToGet))
-            .objects();
-
-        console.log(data_g)
-
-        return data_g;
+    const result = variableToGetArray.reduce((acc, cur, i) => {
+        
+      if (!acc) {  
+        const filteredTable = data
+        .filter(aq.escape((d) => d.year >= yearFrom && d.variable === cur)) 
+        .select("country", "value", "year", "variable")
+        // console.log(filteredTable.objects())
+        return filteredTable        
     }
-    else {
-        return []
+      else {
+        const filteredTable = data.filter(aq.escape((d) => d.year >= yearFrom && d.variable === cur))
+        .select("country", "value", "year", "variable"); 
+        // console.log(filteredTable.objects())
+        return acc.join_full(filteredTable, [["year"], ["year"]])
+
     }
+
+    }, null);
+
+    return result.object();
+
 }
 
 async function main() {
 
-    const countryNames = [
-    "AD", "AE", "AF", "AG", "AI", "AL", "AM", "AN", "AO", "AR", "AS", "AT", "AU", "AZ", "AW", 
-    "BA", "BB", "BD", "BE", "BF", "BG", "BH", "BI", "BJ", "BL", "BM", "BN", "BO", "BQ", "BR", "BS", "BT", "BW",  "BY", "BZ", 
-    "CA", "CD", "CF", "CG", "CH", "CI", "CK", "CL", "CM", "CN", "CO", "CR", "CU", "CV", "CY", "CZ", "CW",
-    "DE", "DJ", "DK", "DO", "DM", "DZ", "EC", "EE", "EG", "EH", "ER", "ES", "ET", "FI", "FJ", "FK", "FM", "FO", "FR", 
-    "GA", "GB", "GD", "GE", "GG", "GH", "GI", "GL", "GM", "GN", "GQ", "GR", "GT", "GU", "GW", "GY", 
-    "HK", "HN", "HR", "HT", "HU", "ID", "IE", "IL", "IN", "IM", "IQ", "IR", "IS", "IT", "JM", "JO", "JP", 
-    "KI", "KE", "KG", "KH", "KM", "KN", "KP", "KR", "KS", "KW", "KY", "KZ",
-    "LA", "LB", "LC", "LI", "LK", "LR", "LS", "LT", "LU", "LV", "LY",
-    "MA", "MC", "MD", "ME", "MH", "MG", "MK", "ML", "MP", "MM", "MN", "MO", "MR", "MS", "MT",
-    "MU", "MV", "MW", "MX", "MY", "MZ", "NA", "NC", "NE", "NG", "NI", "NL",
-    "NO", "NP", "NZ", "OM", "PA", "PE", "PG", "PH", "PK", "PL", "PR",
-    "PS", "PT", "PW", "PY", "QA", "RO", "RS", "RU", "RW", "SA", "SB", 
-    "SC", "SD", "SE", "SG", "SH", "SI", "SK", "SL", "SM", "SN", "SO",
-    "SR", "SS", "ST", "SU", "SV", "SX", "SY", "SZ", "TD", "TG", "TH",
-    "TJ", "TL", "TM", "TN", "TR", "TT", "TW", "TZ", "UA", "UG", "US",
-    "UY", "UZ", "VE", "VN", "VU", "YE", "YU", "ZA", "ZM", "ZW", "ZZ"
+    const countryNames = [ "AU",  "DE",
+    // "AD", "AE", "AF", "AG", "AI", "AL", "AM", "AN", "AO", "AR", "AS", "AT", "AU", "AZ", "AW", 
+    // "BA", "BB", "BD", "BE", "BF", "BG", "BH", "BI", "BJ", "BL", "BM", "BN", "BO", "BQ", "BR", "BS", "BT", "BW", "BY", "BZ", 
+    // "CA", "CD", "CF", "CG", "CH", "CI", "CK", "CL", "CM", "CN", "CO", "CR", "CS", "CU", "CV", "CY", "CZ", "CW",
+    // "DE", "DJ", "DK", "DO", "DM", "DZ", "EC", "EE", "EG", "EH", "ER", "ES", "ET", "FI", "FJ", "FK", "FM", "FO", "FR", 
+    // "GA", "GB", "GD", "GE", "GG", "GH", "GI", "GL", "GM", "GN", "GQ", "GR", "GT", "GU", "GW", "GY", 
+    // "HK", "HN", "HR", "HT", "HU", "ID", "IE", "IL", "IN", "IM", "IQ", "IR", "IS", "IT", "JM", "JO", "JP", 
+    // "KI", "KE", "KG", "KH", "KM", "KN", "KP", "KR", "KS", "KW", "KY", "KZ",
+    // "LA", "LB", "LC", "LI", "LK", "LR", "LS", "LT", "LU", "LV", "LY",
+    // "MA", "MC", "MD", "ME", "MH", "MG", "MK", "ML", "MP", "MM", "MN", "MO", "MR", "MS", "MT",
+    // "MU", "MV", "MW", "MX", "MY", "MZ", "NA", "NC", "NE", "NG", "NI", "NL",
+    // "NO", "NP", "NZ", "OM", "PA", "PE", "PF", "PG", "PH", "PK", "PL", "PM", "PR",
+    // "PS", "PT", "PW", "PY", "QA", "QB", "RO", "RS", "RU", "RW", "SA", "SB", 
+    // "SC", "SD", "SE", "SG", "SH", "SI", "SK", "SL", "SM", "SN", "SO",
+    // "SR", "SS", "ST", "SU", "SV", "SW", "SX", "SY", "SZ", "TC", "TD", "TG", "TH",
+    // "TJ", "TK", "TL", "TM", "TN", "TR", "TT", "TV", "TW", "TZ", "UA", "UG", "US",
+    // "UY", "UZ", "VA", "VC", "VE", "VG", "VI", "VN", "VU", "WF", "WO", "WS",
+    // "XS", "YE", "YU", "ZA", "ZM", "ZW", "ZZ"
 ];
 
-    const promises = countryNames.map(d => fetchAndTransform(d, "gdiincj992", "1980"))
+    const promises = countryNames.map(d => fetchAndTransform(d, ["npopuli999", "xlcyuxi999", "kifghgi999"], "1980"))
 
     const result = await Promise.all(promises)
 
     let data_total = result.flatMap(d => d);
     console.log(data_total);
 
-    const data_total_csv = d3.csvFormat(data_total, ["year", "country", "variable", "value"]);
-    console.log(data_total_csv);
+    // const data_total_csv = d3.csvFormat(data_total, ["year", "country", "variable", "value"]);
+    // console.log(data_total_csv);
 }
 
 main();
