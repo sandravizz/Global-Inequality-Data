e3025d2c90c870c76ebce4d2b80d7336d4a1fbcf
Sandra Becker <sandra.becker1982@gmail.com>
2024-08-02 12:21:30 +0100

    adding wrangeling js

--- js/wrangeling.js
+++ js/wrangeling.js
@@ -1,3 +1,41 @@
+const aq = require('arquero')
+const fs = require('fs-extra')
+const path = require('path')
+const d3 = require('d3')
+
+async function proceesing() {
+    console.log('---> start processing5')
+
+    const data_all = await aq.load(
+        `../js/data_all.csv`,
+        { delimiter: ',' },
+        { using: aq.fromCSV }
+    )
+    console.log(data_all)
+
+    const gini_1980_2022 = data_all
+        .groupby('shortname', 'region', 'country')
+        .pivot('year', 'gini_pretaxes')
+        .select('1980', '2022', 'shortname', 'region', 'country')
+        .objects()
+
+    console.log(gini_1980_2022)
+
+    // Convert tidyData to CSV format
+    const csvData = d3.csvFormat(gini_1980_2022)
+
+    // Write CSV data to file
+    const outputPath = path.resolve(__dirname, './gini_1980_2022.csv')
+    fs.ensureDirSync(path.dirname(outputPath))
+    fs.writeFileSync(outputPath, csvData, 'utf8')
+
+    console.log(`CSV file has been written to ${outputPath}`)
+
+    console.log('<--- finsished proceesing')
+}
+
+proceesing()
+
 // async function loading3() {
 
 //     console.log('---> start processing')
@@ -37,26 +75,3 @@
 // };
 
 // loading4();
-
-async function loading5() {
-    console.log('---> start processing5')
-
-    const gini_income_complete = await aq.load(
-        `../data/output_files/income/3.csv`,
-        { delimiter: ',' },
-        { using: aq.fromCSV }
-    )
-    console.log(gini_income_complete)
-
-    const gini_1980_2022 = gini_income_complete
-        .groupby('country', 'region')
-        .pivot('year', 'value')
-        .select('1980', '2022', 'country', 'region')
-        .objects()
-
-    console.log(gini_1980_2022)
-
-    console.log('<--- finsished processing5')
-}
-
-loading5()
